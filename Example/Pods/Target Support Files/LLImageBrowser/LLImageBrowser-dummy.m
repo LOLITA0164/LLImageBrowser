d793946322ee10ee6d4c15adbce0ef7d65eefd6f
LOLITA0164 <476512340@qq.com>
2019-12-20 08:49:57 +0800

    修改文件名称

---
+++ Example/Pods/Target Support Files/LLImageBrowser/LLImageBrowser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LLImageBrowser : NSObject
+@end
+@implementation PodsDummy_LLImageBrowser
+@end
---
+++ LLImageBrowser/Classes/LLImageBrowser.h
@@ -0,0 +1,65 @@
+//
+//  LLImageBrowser.h
+//  LLImageBrowser
+//
+//  Created by LOLITA on 2017/8/31.
+//  Copyright © 2017年 LOLITA0164. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LLImageTool.h"
+
+@class LLImageBrowser;
+@protocol LLImageBrowserDelegate <NSObject>
+@optional
+/**
+ @param photoBrowser 图片浏览器
+ @param currentPage 当前图片下标
+ */
+/// 主线程
+-(UIView *)photoBrowser:(LLImageBrowser *)photoBrowser didScrollToPage:(NSInteger)currentPage;
+/// 配合视图滚动使用
+-(void)photoBrowser:(LLImageBrowser *)photoBrowser didScrollToPage:(NSInteger)currentPage completion:(void(^)(UIView *))completion;
+/// 结束显示
+-(void)photoBrowser:(LLImageBrowser *)photoBrowser didHidden:(NSInteger)currentPage;
+/// 长按事件
+-(void)photoBrowser:(LLImageBrowser *)photoBrowser longPress:(UILongPressGestureRecognizer*)longPress;
+@end
+
+
+
+
+@interface LLImageBrowser : UIView
+@property (weak, nonatomic) id <LLImageBrowserDelegate> delegate;
+@property (copy, nonatomic) void(^didHiddenBlock)(void);
+@property (assign ,nonatomic, readonly) NSInteger currentPage;      // 当前页
+
+/**
+ 加载本地图片
+ @note selectedView 可选参数：一般来说是一个imageView，关闭浏览器时会将这个视图移回原来的位置
+ */
++(instancetype)showLocalImages:(NSArray*)images selectedIndex:(NSInteger)index selectedView:(UIView *)selectedView;;
+
+/**
+ 加载网络图片
+ @note selectedView 可选参数：一般来说是一个imageView，关闭浏览器时会将这个视图移回原来的位置
+ */
++(instancetype)showURLImages:(NSArray*)images placeholderImage:(UIImage *)image selectedIndex:(NSInteger)index  selectedView:(UIView *)selectedView;
+
+/// 主动隐藏
+-(void)hidden;
+
+@end
+
+
+
+
+
+@interface PhotoBrowserCollectionViewCell : UICollectionViewCell
+@property(nonatomic,strong)UIImageView *imageView;
+@property (strong ,nonatomic) UIScrollView *scrollView;
+@end
+
+
+
+
